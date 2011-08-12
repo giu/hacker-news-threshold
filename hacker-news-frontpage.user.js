fe48888002dd269cde0268c6cc9f8efee0a02d86
giu <public@giu.me>
2011-08-12 12:22:51 +0200

    Added error highlighting

--- hacker-news-frontpage.user.js
+++ hacker-news-frontpage.user.js
@@ -9,11 +9,13 @@
 // @require			http://code.jquery.com/jquery.min.js
 // ==/UserScript==
 $(function(){
-	$("head").append("<style>.hnfph{background: #FFFB93 !important;}</style>");
+	$("head").append("<style>.hnth{background: #FFFB93 !important;}.hnterr{background:#FF0000;color:#fff;}</style>");
 
 	function highlightThreads(threshold){
+		var $hnfti = $("#hnfti");
 		if(!isNaN(threshold)){
-			$(".hnfph").removeClass("hnfph");
+			$hnfti.removeClass("hnterr");
+			$(".hnth").removeClass("hnth");
 			$("span[id^='score_']").filter(function(){
 				var m = $(this).html().match(/[0-9]+/g);
 
@@ -24,17 +26,21 @@ $(function(){
 					return false;
 				}
 			})
-			.closest("tr").prev().addClass("hnfph");
+			.closest("tr").prev().addClass("hnth");
+		}
+		else{
+			$(".hnth").removeClass("hnth");
+			$hnfti.addClass("hnterr");
 		}
 	}
 
-	$("#hnf-point-threshold").live("keyup", function(){
+	$("#hnfti").live("keyup", function(){
 		highlightThreads($(this).val());
 	});
 
 	var startthreshold = 100;
 
-	$($(".pagetop")[0]).append(" | Set threshold: <input type='text' id='hnf-point-threshold' value='"+startthreshold+"'  />");
+	$($(".pagetop")[0]).append(" | Threshold: <input type='text' id='hnfti' value='"+startthreshold+"'  />");
 
 	highlightThreads(startthreshold);
 });
